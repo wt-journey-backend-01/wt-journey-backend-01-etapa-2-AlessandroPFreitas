61b7e2f4807bd5c8e8b53b1e90e03b32e4ae033c
AlessandroPFreitas <164437174+AlessandroPFreitas@users.noreply.github.com>
2025-07-30 19:25:34 -0300

    Remove dotenv configuration and set PORT directly in server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
-app.use(express.json());
+const PORT =  3000;
 const casosRouter = require("./routes/casosRoutes");
 const agentesRoutes = require("./routes/agentesRoutes");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./docs/swagger.json");
 
-const PORT = process.env.PORT || 3000;
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
 app.use(casosRouter);
 app.use(agentesRoutes);
 
